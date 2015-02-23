d6ce7c472508d79142dff274b458b71cdca1e1b1
Jason Brownbridge <jbrownbridge@gmail.com>
2015-02-23 11:15:58 +0200

    More efficient package installation.

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 if [ -e packages.txt ] ; then
   DEBIAN_FRONTEND=noninteractive
-  while read p; do
-    apt-get -y --no-install-recommends install $p
-  done < packages.txt
+  cat packages.txt | apt-get -y --no-install-recommends install
 fi
 if [ -e requirements.txt ] ; then
   while read r; do
