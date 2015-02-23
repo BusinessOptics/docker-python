62571f8eaa51e2d3e3406e18e0a08d702c60795b
Jason Brownbridge <jbrownbridge@gmail.com>
2015-02-23 12:18:52 +0200

    Add a pre-pip hook.

--- install_requirements.sh
+++ install_requirements.sh
@@ -3,6 +3,10 @@ if [ -e packages.txt ] ; then
   DEBIAN_FRONTEND=noninteractive
   cat packages.txt | apt-get -y --no-install-recommends install
 fi
+# Allows apps to install custom stuff before pip runs.
+if [ -e pre-pip.sh ] ; then
+  ./pre-pip.sh
+fi
 if [ -e requirements.txt ] ; then
   while read r; do
     pip install -U $r
